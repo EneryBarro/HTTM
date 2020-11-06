2237aa8c47a3cb11cfd654728f98e7e8d4010fac
EneryBarro <deninbog1406@gmail.com>
2020-11-06 08:13:47 +0200

    Version_01

---
+++ lab03/test3021.c
@@ -0,0 +1,10 @@
+int main()
+{
+    int first=2, second, third, sixth;
+    second=first*first;
+    third=second*first;
+    sixth=third*third;
+printf("%i\n",sixth);
+return 0;
+}
+    //В результате получаем число 2 в степени 6.
---
+++ lab03/test303.c
@@ -0,0 +1,11 @@
+int main ()
+{
+    int first = 2, second, fourth, fifth, tenth;
+    second=first*first;
+    fourth=second*second;
+    fifth=fourth*first;
+    tenth=fifth*fifth; 
+  printf("%i\n", tenth );
+return 0;
+}
+    //В результате получаем число 2 в степени 10.
