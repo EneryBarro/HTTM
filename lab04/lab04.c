26143d7b61755e47808b3a5167c2493751f93638
EneryBarro <deninbog1406@gmail.com>
2020-11-11 09:01:25 +0200

    Version_01

--- lab04/lab04.c
+++ lab04/lab04.c
@@ -1,12 +1,20 @@
- int main() {
-    int k,m,n;
-    scanf("%d%d%d",&k,&m,&n);
+ int main() 
+ {
  
-    if (k > m)
-        if (k > n) printf("Result: %d \n", k);
-        else printf("Result: %d \n", n);
-    else
-        if (m > n) printf("Result: %d \n", m);
-        else printf("Result: %d \n", n);
-        return 0;
+ int k,m,n;
+ 
+    
+ printf("Enter your numbers:\n");
+ scanf("%d%d%d",&k,&m,&n);
+ 
+ 
+ if (k > m)
+     if (k > n) printf("Max number: %d \n", k);
+     else printf("Max number: %d \n", n);
+ else
+     if (m > n) printf("Max number: %d \n", m);
+     else printf("Max number: %d \n", n);
+     
+        
+ return 0;
 }
